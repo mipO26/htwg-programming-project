46407e5e70ab31bb5b6fd2d53b935ea319d620d0
slurfer <dousek.martin@gmail.com>
2026-06-14 22:46:08 +0200

    simplyfi imports

--- src/audio/audio.h
+++ src/audio/audio.h
@@ -2,7 +2,8 @@
 #define SOUND_H
 
 #include <SDL.h>
-#include "audioInterface.h"
+#include <stdbool.h>
+#include "notes.h"
 
 #define False 0
 #define SAMPLE_RATE 44100
@@ -10,5 +11,11 @@
 /* Sound system */
 SDL_AudioDeviceID audio_init();
 void audio_terminate(SDL_AudioDeviceID device);
+
+
+/* Notes api */
+void playNote(Note note, double duration);
+void playNoteMs(Note note, uint32_t duration_ms);
+void stopNote(Note note);
 
 #endif /* SOUND_H */
--- src/audio/audioInterface.h
+++
@@ -1,8 +0,0 @@
-#pragma once
-
-#include <stdbool.h>
-#include "notes.h"
-
-void playNote(Note note, double duration);
-void playNoteMs(Note note, uint32_t duration_ms);
-void stopNote(Note note);
