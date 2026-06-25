93cabe634152a1b2f63f12e2f2165eb175979b32
slurfer <dousek.martin@gmail.com>
2026-06-25 17:36:59 +0200

    Revert "presentation"
    This reverts commit 0741287468ece880cf22ff039d34f0e174c20ae5.

--- src/main.c
+++ src/main.c
@@ -19,27 +19,26 @@ int main(void)
     /*==================================================
     * APP
     *==================================================*/
-    play_song();
-    // printf("\n\n\n\n\n");
-    // printf("┏━━━━━━━━━━━━━━━━━━┓\n");
-    // printf("┃ 𝗦𝗢𝗡𝗔𝗧𝗘 𝗜𝗡 𝗖-𝗖𝗢𝗗𝗘 ┃\n");
-    // printf("┗━━━━━━━━━━━━━━━━━━┛\n");
-    // printf("\n");
-    // ui();
-    // printf("\n\n");
-    // char opt;
-    // do {
-    //     printf("What do you like to do today?\n\t1] Play predefined song\n\t2] Play custom chord\n\t3] Exit...\n");
-    //     scanf(" %c", &opt);
-    //     emptyAllSchedulers();
-    //     switch (opt)
-    //     {
-    //         case '1': play_song(); break;
-    //         case '2': keyboard(); break;
-    //         case '3': break;
-    //         default: printf("Wrong input\n"); break;
-    //     }
-    // } while (opt != '3');
+    printf("\n\n\n\n\n");
+    printf("┏━━━━━━━━━━━━━━━━━━┓\n");
+    printf("┃ 𝗦𝗢𝗡𝗔𝗧𝗘 𝗜𝗡 𝗖-𝗖𝗢𝗗𝗘 ┃\n");
+    printf("┗━━━━━━━━━━━━━━━━━━┛\n");
+    printf("\n");
+    ui();
+    printf("\n\n");
+    char opt;
+    do {
+        printf("What do you like to do today?\n\t1] Play predefined song\n\t2] Play custom chord\n\t3] Exit...\n");
+        scanf(" %c", &opt);
+        emptyAllSchedulers();
+        switch (opt)
+        {
+            case '1': play_song(); break;
+            case '2': keyboard(); break;
+            case '3': break;
+            default: printf("Wrong input\n"); break;
+        }
+    } while (opt != '3');
     
     /*==================================================
     * TERMINATION
